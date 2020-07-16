e1952891d9af6ecae20258761d969557271e573e
Rehman Ali <rehmanali.9442289@gmail.com>
2020-07-16 19:29:28 +0500

    FINAL: Final version

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 from telethon import TelegramClient, events, errors
+from telethon.events import StopPropagation
 from configparser import ConfigParser
 import json
 import socks
+import re
 # import logging
 
 
@@ -20,6 +22,7 @@ target_group = config['TELEGRAM'].getint('telegram_destination_group_id')
 session_file = 'telegramBot'
 
 
+sent_msgs = 0
 # ##############################################< Proxy >##############################################
 
 try:
@@ -27,8 +30,7 @@ try:
     proxy_server = config['PROXY']['server'].encode()
     proxy_port = config['PROXY'].getint('port')
 except KeyError:
-    # Proxy is always disabled yet!
-    proxy_enabled = False
+    proxy_enabled = True
     proxy_server = '159.89.49.60'
     proxy_port = 31264
     pass
@@ -52,17 +54,46 @@ if proxy_enabled:
 else:
     telegramClient = TelegramClient(session_file, api_id, api_hash)
 
-chats = json.loads(config.get("TELEGRAM", "source_channels"))
 dest_channel = config['TELEGRAM'].getint('destination_channel')
+chats = json.loads(config.get("TELEGRAM", "source_channels"))
+filtered_channels = json.loads(config.get("TELEGRAM", "filtered_channels"))
+
+
+def deEmojify(text):
+    # A python function to remove emojis from string
+    regrex_pattern = re.compile(pattern="["
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                "]+", flags=re.UNICODE)
+    return regrex_pattern.sub(r'', text)
 
 
 @telegramClient.on(events.NewMessage(chats, blacklist_chats=False))
 async def newMessageHandler(msg):
     await telegramClient.send_message(dest_channel, msg.message)
+    global sent_msgs
+    sent_msgs += 1
+    print(f'[+] Forwarded Messages: {sent_msgs}', end='\r')
+    raise StopPropagation
+
+
+@telegramClient.on(events.NewMessage(chats=filtered_channels, blacklist_chats=False))
+async def filteredMessageHandler(msg):
+    global num_of_msgs
+    if msg.message.media:
+        raise StopPropagation
+    emojiFiltered = deEmojify(msg.message.message)
+    await telegramClient.send_message(dest_channel, emojiFiltered)
+    global sent_msgs
+    sent_msgs += 1
+    print(f'[+] Forwarded Messages: {sent_msgs}', end='\r')
+    raise StopPropagation
 
 
 try:
-    telegramClient.start()
+    telegramClient.start(phone='+923089442289')
     print("-------------------------\nMessage Forward bot is up!\n-------------------------\n")
     telegramClient.run_until_disconnected()
 except KeyboardInterrupt:
